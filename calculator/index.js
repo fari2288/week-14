bb2d8d7f5fdac518a6e167a6cebea4a9fb1b60db
Fariza Kubanova <farizatkubanova@gmail.com>
2021-09-17 02:28:40 +0300

    week 14

---
+++ calculator/index.js
@@ -0,0 +1,31 @@
+    function showMessage() {
+        let name = prompt('Как Вас зовут?');
+        alert(`Привет, ${name}!`);
+    }
+
+    function getSum() {
+        let x = prompt('Первое число');
+        let y = prompt('Второе число');
+        alert(Number(x)+Number(y));
+    }
+
+    function getMinus() {
+        let x = prompt('Первое число');
+        let y = prompt('Второе число');
+        alert(Number(x)-Number(y));
+    }
+
+    function getDivide() {
+        let x = prompt('Первое число');
+        let y = prompt('Второе число');
+        alert(Number(x)/Number(y));
+    }
+
+    function getTimes() {
+        let x = prompt('Первое число');
+        let y = prompt('Второе число');
+        alert(Number(x)*Number(y));
+    }
+
+
+
---
+++ gallery/gallery.js
@@ -0,0 +1,4 @@
+function changeMe() {
+    let catImage = document.getElementById('catImage');
+    catImage.src = "https://bugaga.ru/uploads/posts/2017-01/1483531805_dvulikaya-koshka-yana-8.jpg";
+}
